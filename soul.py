1e5558c8446e5f3275d6fc6d2221c431a9a8b3ea
bisht1397 <tapatapgaminf@gmail.com>
2025-01-21 17:24:36 +0530

    Update soul.py

--- soul.py
+++ soul.py
@@ -18,7 +18,7 @@ loop = asyncio.get_event_loop()
 TOKEN = '7670606668:AAEPBT7ft9R-2MrmXeHW-DKVTmwSLUCbTxc'
 ADMIN_USER_ID =5634497189
 MONGO_URI = 'mongodb+srv://sharp:sharp@sharpx.x82gx.mongodb.net/?retryWrites=true&w=majority&appName=SharpX'
-USERNAME = "@VIPMODSXADMIN"  # Immutable username for maximum security
+USERNAME = "@RICHGUTAM"  # Immutable username for maximum security
 
 # Attack Status Variable to Control Single Execution
 attack_in_progress = False
